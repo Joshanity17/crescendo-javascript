cf8ecd1d725b391d92d8dacec8b6243bddc28791
Joshua Alzate <josh.alzate17@gmail.com>
2021-04-02 12:29:27 +0800

    Added Routing Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeResolver } from 'src/shared/Recipe.resolver';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipeListComponent,
+    resolve: {
+      recipes: RecipeResolver
+    }
+  },
+  {
+    path: 'details/:uuid',
+    component: RecipeDetailsComponent,
+    resolve: {
+      details: RecipeResolver
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
